c3abea0765ee7518ac6749667423a3a3efcd83bf
Kunalgrover27 <kunalgrover.2704@gmail.com>
2024-01-16 15:21:16 +0530

    route created

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import express from "express"
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
@@ -13,6 +13,14 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(cookieParser())
+
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+
+
+//route declaration
+app.use("/api/v1/users", userRouter)
 
 
 export {app}
