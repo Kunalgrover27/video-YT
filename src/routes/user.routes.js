62a5dbbe1bcbd0800a7812ef65b52e3557ef8a57
Kunalgrover27 <kunalgrover.2704@gmail.com>
2024-01-18 16:22:30 +0530

    added more api to user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -24,4 +24,11 @@ router.route("/register").post(
     //secure routes
     router.route('/logout').post(verifyJWT, logoutUser)
     router.route('/refresh-Token').post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
     export default router
