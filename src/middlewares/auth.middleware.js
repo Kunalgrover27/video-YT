b0036f893b630e14f4b8eba3b72e9e2861a8db52
Kunalgrover27 <kunalgrover.2704@gmail.com>
2024-01-25 17:35:39 +0530

    cmnt

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -10,7 +10,7 @@ export const verifyJWT = asyncHandler(async(rea,res,next) => {
         if(!token){
             throw new ApirError(401,"unauthorized request")
         }
-
+        // for github repo
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
 
